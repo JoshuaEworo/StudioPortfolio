f07cce5455db0573eb335d69735576fc579585df
Joshua Eworo <eworojoshua@gmail.com>
2024-07-26 02:41:16 -0500

    fixed otto's arena link

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -75,7 +75,7 @@ export const load: LayoutLoad = async ({ url, data, depends, fetch }) => {
                 name : "Otto's Arena",
                 type : "C++ Program | Game Development",
                 img : "https://images.unsplash.com/photo-1525711857929-4272fb4a040f?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                link: "/"
+                link: "https://replit.com/@JoshuaEworo1/OttosArenaRPG"
             },
         ]
   }
